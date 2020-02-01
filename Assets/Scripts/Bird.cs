8d08263e1f335edf57e9a5a2a3f4d9ec7dc3ee33
guangdao <chenhuanming.cn@gmail.com>
2020-02-01 22:42:50 +0800

    init

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb2d;
+    private float upForce = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameControl.instance.gameOver == false)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb2d.AddForce(new Vector2(0f, upForce));
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameControl.instance.GameOver();
+    }
+
+}
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    void Awake()
+    {
+        
+        if(instance == null)
+        {
+            instance = this;
+        }else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+        gameOverText.SetActive(true);
+    }
+}
